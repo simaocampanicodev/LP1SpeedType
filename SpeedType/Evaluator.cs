51c5589527da4affc0a2bdff2162ff2e8fe86a1d
txger. <simaocampanico.dev@gmail.com>
2025-04-11 14:12:38 +0100

    Evaluator.cs is done

--- SpeedType/Evaluator.cs
+++ SpeedType/Evaluator.cs
@@ -25,7 +25,7 @@ namespace SpeedType
         /// </remarks>
         public double CalculateWPM(string userInput, double timeTaken)
         {
-            int wordsTyped = // ////////// => TO IMPLEMENT <= //////////// //
+            int wordsTyped = userInput.Split(" ").Length;
             return (wordsTyped / timeTaken) * 60;
         }
 
@@ -51,11 +51,12 @@ namespace SpeedType
         public int CalculateAccuracy(string userInput, string originalText)
         {
             int correctChars = 0;
-            int minLength = // ////////// => TO IMPLEMENT <= //////////// //
-
+            int minLength = originalText.Length;
+            
             for (int i = 0; i < minLength; i++)
             {
-                correctChars++;
+                if (userInput.Length <= i) break;
+                if (originalText[i].Equals(userInput[i])) correctChars++;
             }
 
             return (int)((double)correctChars / originalText.Length * 100);
